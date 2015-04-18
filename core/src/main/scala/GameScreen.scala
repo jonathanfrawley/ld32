c50fc3e02f7cc208767b1fa6701056b9d5369be2
Jonathan Frawley <jmf@9lines.org>
2015-04-18 14:09:06 +0100

    Granny simple movement

--- core/src/main/scala/GameScreen.scala
+++ core/src/main/scala/GameScreen.scala
@@ -1,7 +1,7 @@
 package ludumdare32
 import com.badlogic.gdx.Input.Keys
 import com.badlogic.gdx.{Gdx, Screen}
-import com.badlogic.gdx.graphics.g2d.{Animation, BitmapFont, SpriteBatch, TextureRegion}
+import com.badlogic.gdx.graphics.g2d._
 import com.badlogic.gdx.graphics.{Color, GL20, OrthographicCamera, Texture}
 import com.badlogic.gdx.math.{MathUtils, Rectangle, Vector3}
 import com.badlogic.gdx.utils.TimeUtils
@@ -9,40 +9,50 @@ import com.badlogic.gdx.utils.TimeUtils
 import scala.collection.mutable.ArrayBuffer
 
 class GameScreen (game: LudumDareSkeleton) extends Screen {
+  /*
   lazy val dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"))
   lazy val rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"))
   lazy val dropImage = new Texture(Gdx.files.internal("droplet.png"))
-  lazy val bucketImage = new Texture(Gdx.files.internal("bucket.png"))
+  */
+  lazy val grannyImage = new Texture(Gdx.files.internal("granny.png"))
   lazy val camera = new OrthographicCamera()
-  lazy val batch = new SpriteBatch()
-  var bucket = new Rectangle()
-  var raindrops = new ArrayBuffer[Rectangle]()
-  var lastDropTime : Long = 0
+  lazy val game.batch = new SpriteBatch()
+  //lazy var granny = new Sprite(grannyImage)
+  lazy val granny = new Rectangle()
+  //var raindrops = new ArrayBuffer[Rectangle]()
+  //var lastDropTime : Long = 0
   //Animation
   val FRAME_COLS = 6
   val FRAME_ROWS = 5
+  /*
   var walkAnimation : Animation = null
   var walkSheet : Texture = null
   var walkFrames : com.badlogic.gdx.utils.Array[TextureRegion] = null
   var spriteBatch : SpriteBatch = null
+  */
   var currentFrame : TextureRegion = null
   var stateTime : Float = 0
   //fonts
   var font : BitmapFont = null
 
+  /*
   // start the playback of the background music immediately
   rainMusic.setLooping(true)
   rainMusic.play()
+  */
+  val gameHeight = 480
+  val gameWidth = 800
 
-  camera.setToOrtho(false, 800, 480)
+  camera.setToOrtho(false, gameWidth, gameHeight)
 
-  bucket.x = 800 / 2 - 64 / 2
-  bucket.y = 20
-  bucket.width = 64
-  bucket.height = 64
+  granny.width = 27
+  granny.height = 51
+  granny.x = gameWidth / 2 - granny.width / 2
+  granny.y = 20
 
-  spawnRaindrop()
+  //spawnRaindrop()
 
+  /*
   //Animation stuff
   walkSheet = new Texture(Gdx.files.internal("animation_sheet.png"))
   val tmp = TextureRegion.split(walkSheet, walkSheet.getWidth()/FRAME_COLS, walkSheet.getHeight()/FRAME_ROWS)
@@ -58,6 +68,7 @@ class GameScreen (game: LudumDareSkeleton) extends Screen {
   walkAnimation = new Animation(0.025f, walkFrames)
   spriteBatch = new SpriteBatch()
   stateTime = 0f
+  */
 
   //fonts
   font = new BitmapFont()
@@ -68,34 +79,44 @@ class GameScreen (game: LudumDareSkeleton) extends Screen {
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
     camera.update()
 
-    batch.setProjectionMatrix(camera.combined)
-    batch.begin()
-    batch.draw(bucketImage, bucket.x, bucket.y)
-    batch.end()
+    game.batch.setProjectionMatrix(camera.combined)
+    game.batch.begin()
+    //game.batch.draw(grannyImage, granny.x, granny.y)
+    game.batch.draw(grannyImage, granny.x, granny.y, granny.width, granny.height)
+    game.batch.end()
 
     //Handle input
+    /*
     if(Gdx.input.isTouched) {
       val touchPos = new Vector3
       touchPos.set(Gdx.input.getX, Gdx.input.getY, 0)
       camera.unproject(touchPos)
-      bucket.x = touchPos.x - 64 / 2
+      granny.x = touchPos.x - 64 / 2
     }
-    if(Gdx.input.isKeyPressed(Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime
-    if(Gdx.input.isKeyPressed(Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime
+    */
+    val grannyXSpeed = 300
+    val grannyYSpeed = 200
+    if(Gdx.input.isKeyPressed(Keys.A)) granny.x -= grannyXSpeed * Gdx.graphics.getDeltaTime
+    if(Gdx.input.isKeyPressed(Keys.D)) granny.x += grannyXSpeed * Gdx.graphics.getDeltaTime
+    if(Gdx.input.isKeyPressed(Keys.S)) granny.y -= grannyYSpeed * Gdx.graphics.getDeltaTime
+    if(Gdx.input.isKeyPressed(Keys.W)) granny.y += grannyYSpeed * Gdx.graphics.getDeltaTime
 
     // Stay within limits
-    if(bucket.x < 0) bucket.x = 0
-    if(bucket.x > 800 - 64) bucket.x = 800 - 64
+    if(granny.x < 0) granny.x = 0
+    if(granny.x > gameWidth - granny.width)  granny.x = gameWidth - granny.width
+    if(granny.y < 0) granny.y = 0
+    if(granny.y > gameHeight - granny.height) granny.y = gameHeight - granny.height
 
-    if(TimeUtils.nanoTime - lastDropTime > 1000000000) spawnRaindrop()
+    //if(TimeUtils.nanoTime - lastDropTime > 1000000000) spawnRaindrop()
 
+    /*
     //TODO: Figure out a nicer way to do this
     var newRainDrops = new ArrayBuffer[Rectangle]()
     for(raindrop <- raindrops) {
       raindrop.y -= 200 * Gdx.graphics.getDeltaTime()
       var remove = false
 
-      if(raindrop.overlaps(bucket)) {
+      if(raindrop.overlaps(granny)) {
         dropSound.play()
         remove = true
       }
@@ -106,44 +127,55 @@ class GameScreen (game: LudumDareSkeleton) extends Screen {
 
     }
     raindrops = newRainDrops
+    */
 
-    batch.begin()
-    batch.draw(bucketImage, bucket.x, bucket.y)
+    /*
+    game.batch.begin()
+    game.batch.draw(grannyImage, granny.x, granny.y)
     for(raindrop <- raindrops) {
       batch.draw(dropImage, raindrop.x, raindrop.y)
     }
-    batch.end()
+    game.batch.end()
+    */
 
+    /*
     // Animation stuff
     stateTime += Gdx.graphics.getDeltaTime()
     currentFrame = walkAnimation.getKeyFrame(stateTime, true)
     spriteBatch.begin()
     spriteBatch.draw(currentFrame, 50, 50)
     spriteBatch.end()
+    */
 
     // font
-    batch.begin()
-    font.draw(batch, "Hello World", 200, 200)
-    batch.end()
+    /*
+    game.batch.begin()
+    font.draw(game.batch, "Hello World", 200, 200)
+    game.batch.end()
+    */
 
   }
 
   override def dispose() {
+    grannyImage.dispose()
+    /*
     dropImage.dispose()
-    bucketImage.dispose()
     dropSound.dispose()
     rainMusic.dispose()
     batch.dispose()
+    */
   }
 
   def spawnRaindrop() {
+    /*
     val raindrop = new Rectangle()
-    raindrop.x = MathUtils.random(0, 800-64)
-    raindrop.y = 480
+    raindrop.x = MathUtils.random(0, gameWidth-64)
+    raindrop.y = gameHeight
     raindrop.width = 64
     raindrop.height = 64
     raindrops += raindrop
     lastDropTime = TimeUtils.nanoTime()
+    */
   }
 
   override def resize(width: Int, height: Int): Unit = {}
